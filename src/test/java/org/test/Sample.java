b369226ed63ebe322fcd8b9cd8695aa61a6e8269
vigneshvicky171 <vignesh.vickyms171@gmail.com>
2022-09-07 18:49:08 +0530

    Vignesh's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class Sample {
+	public static void main(String[] args) {
+		System.out.println("Test 1");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+	}
 
 }
